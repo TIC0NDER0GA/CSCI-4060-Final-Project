ff72607c88156f35aa8fb127ff8093764fc3970c
gennyyoung <gyoung9825@gmail.com>
2023-04-23 14:28:57 -0400

    Nav bar added, shopping & purchased lists added, create group button added to register activity

--- app/src/main/java/uga/edu/roomiebudget/PurchasedListActivity.java
+++ app/src/main/java/uga/edu/roomiebudget/PurchasedListActivity.java
@@ -6,21 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-public class ShoppingListActivity extends AppCompatActivity {
+public class PurchasedListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_list);
+        setContentView(R.layout.activity_purchased_list);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.nav_bar, menu);
-        MenuItem shopList = menu.findItem(R.id.shopListItem);
-        shopList.setVisible(false);
+        MenuItem purchList = menu.findItem(R.id.purchListItem);
+        purchList.setVisible(false);
         return true;
     }
 
@@ -28,8 +27,8 @@ public class ShoppingListActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent;
         switch (item.getItemId()) {
-            case R.id.purchListItem:
-                intent = new Intent(this, PurchasedListActivity.class);
+            case R.id.shopListItem:
+                intent = new Intent(this, ShoppingListActivity.class);
                 startActivity(intent);
                 return true;
             case R.id.logoutItem:
@@ -40,4 +39,5 @@ public class ShoppingListActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
 }
