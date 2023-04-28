93ac980f21df49cd73f5995ec027429668ed8295
TIC0NDER0GA <dey60704@uga.edu>
2023-04-28 10:28:11 -0400

    Moved the SimpleData class outside for general use.

--- app/src/main/java/uga/edu/roomiebudget/PurchaseByUserAdapter.java
+++ app/src/main/java/uga/edu/roomiebudget/PurchaseByUserAdapter.java
@@ -65,37 +65,4 @@ public class PurchaseByUserAdapter extends RecyclerView.Adapter<PurchaseByUserAd
     }
 
 
-
-
-
-    private class SimpleData {
-
-        ArrayList<String> titles = new ArrayList<>();
-        ArrayList<LinkedHashMap<String, Double>> entries = new ArrayList<>();
-
-        public SimpleData(LinkedHashMap<String, LinkedHashMap<String,Double>> data) {
-            for (String key : data.keySet()) {
-                titles.add(key);
-                entries.add(data.get(key));
-            }
-        }
-
-
-        public String getTitle(int pos) {
-            return titles.get(pos);
-        }
-
-        public LinkedHashMap<String, Double> getList(int pos) {
-            return entries.get(pos);
-        }
-
-        public int getSize() {
-            return titles.size();
-        }
-
-        public String toString() {
-            return entries.toString() + " " + titles.toString();
-        }
-    }
-
 }
